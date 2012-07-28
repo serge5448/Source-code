4fe3e1b0a0459aa04f557397d6389b4abbe974a8
AdeMiller_cp <SND\AdeMiller_cp>
2012-07-28 13:04:40 -0700

    Finished padding and truncation examples for Ch. 12.

--- Samples/Chapter12/main.cpp
+++ Samples/Chapter12/main.cpp
@@ -146,7 +146,8 @@ void TransposeExample(int matrixSize)
 
     tiled_extent<tileSize, tileSize> computeDomain = inDataView.extent.tile<tileSize, tileSize>();
     accelerator_view view = accelerator(accelerator::default_accelerator).default_view;
-    double elapsedTime = TimeFunc(view, [&]() {
+    double elapsedTime = TimeFunc(view, [&]() 
+    {
         parallel_for_each(computeDomain, [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
         {     
             tile_static unsigned int localData[tileSize][tileSize];
@@ -198,7 +199,8 @@ void TransposePaddedExample(int matrixSize)
     tiled_extent<tileSize, tileSize> computeDomain = inDataView.extent.tile<tileSize, tileSize>();
     computeDomain = computeDomain.pad();
     accelerator_view view = accelerator(accelerator::default_accelerator).default_view;
-    double elapsedTime = TimeFunc(view, [&]() {
+    double elapsedTime = TimeFunc(view, [&]() 
+    {
         parallel_for_each(view, computeDomain, 
             [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
         {     
@@ -238,49 +240,45 @@ void TransposeTruncatedExample(int matrixSize)
     outDataView.discard_data();
 
     tiled_extent<tileSize, tileSize> computeDomain = inDataView.extent.tile<tileSize, tileSize>();
-    tiled_extent<tileSize, tileSize> truncatedDomain = computeDomain.truncate();
+    computeDomain = computeDomain.truncate();
 
     // TODO_AMP: TDRs in release mode on RC build.
     accelerator_view view = accelerator(accelerator::default_accelerator).default_view;
-    double elapsedTime = TimeFunc(view, [&]() {
-        parallel_for_each(truncatedDomain, [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
+    double elapsedTime = TimeFunc(view, [&]() 
+    {
+        parallel_for_each(computeDomain, [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
         {     
             tile_static unsigned int localData[tileSize][tileSize];
             localData[tidx.local[1]][tidx.local[0]] = inDataView[tidx.global];
-
             tidx.barrier.wait();
-
             index<2> outIdx(index<2>(tidx.tile_origin[1], tidx.tile_origin[0]) + tidx.local);
             outDataView[outIdx] = localData[tidx.local[0]][tidx.local[1]];
 
             // Handle tuncated elements using edge thread.
 
-            int idx0, idx1;
-            bool isRightMost = tidx.global[1] == truncatedDomain[1] - 1; 
-            bool isBottomMost = tidx.global[0] == truncatedDomain[0] - 1; 
+            bool isRightMost = tidx.global[1] == computeDomain[1] - 1; 
+            bool isBottomMost = tidx.global[0] == computeDomain[0] - 1; 
 
             // Exit branching as quickly as possible as the majority of threads will not meet the boundary criteria
             if (isRightMost | isBottomMost)
             {
+                int idx0, idx1;
                 if (isRightMost)
                 {
                     idx0 = tidx.global[0]; 
-                    idx1 = truncatedDomain[1];
-                    for (idx1 = truncatedDomain[1]; idx1 < inDataView.extent[1]; idx1++) 
+                    for (idx1 = computeDomain[1]; idx1 < inDataView.extent[1]; idx1++) 
                         outDataView(idx1, idx0) = inDataView(idx0, idx1); 
                 }
-
                 if (isBottomMost)
                 {
                     idx1 = tidx.global[1];
-                    for (idx0 = truncatedDomain[0]; idx0 < inDataView.extent[0]; idx0++) 
+                    for (idx0 = computeDomain[0]; idx0 < inDataView.extent[0]; idx0++) 
                         outDataView(idx1, idx0) = inDataView(idx0, idx1); 
                 }
-
                 if (isRightMost && isBottomMost) 
                 { 
-                    for (idx0 = truncatedDomain[0]; idx0 < inDataView.extent[0]; idx0++)
-                        for (idx1 = truncatedDomain[1]; idx1 < inDataView.extent[1]; idx1++) 
+                    for (idx0 = computeDomain[0]; idx0 < inDataView.extent[0]; idx0++)
+                        for (idx1 = computeDomain[1]; idx1 < inDataView.extent[1]; idx1++) 
                             outDataView(idx1, idx0) = inDataView(idx0, idx1); 
                 }
             }
@@ -308,48 +306,46 @@ void TransposeTruncatedExample2(int matrixSize)
     outDataView.discard_data();
 
     tiled_extent<tileSize, tileSize> computeDomain = inDataView.extent.tile<tileSize, tileSize>();
-    tiled_extent<tileSize, tileSize> truncatedDomain = computeDomain.truncate();
+    computeDomain = computeDomain.truncate();
 
-    const int rightMargin = computeDomain[1] - truncatedDomain[1];
-    const int bottomMargin = computeDomain[0] - truncatedDomain[0];
+    const int rightMargin = computeDomain[1] - computeDomain[1];
+    const int bottomMargin = computeDomain[0] - computeDomain[0];
     accelerator_view view = accelerator(accelerator::default_accelerator).default_view;
-    double elapsedTime = TimeFunc(view, [&]() {
-        parallel_for_each(view, truncatedDomain, [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
+    double elapsedTime = TimeFunc(view, [&]() 
+    {
+        parallel_for_each(view, computeDomain, [=](tiled_index<tileSize, tileSize> tidx) restrict(amp)
         {     
             tile_static unsigned int localData[tileSize][tileSize];
             localData[tidx.local[1]][tidx.local[0]] = inDataView[tidx.global];
-
             tidx.barrier.wait();
-
             index<2> outIdx(index<2>(tidx.tile_origin[1], tidx.tile_origin[0]) + tidx.local);
             outDataView[outIdx] = localData[tidx.local[0]][tidx.local[1]];
 
             // Handle truncated elements using threads in the margins.
 
-            bool isRightMost = tidx.global[1] >= truncatedDomain[1] - rightMargin; 
-            bool isBottomMost = tidx.global[0] >= truncatedDomain[0] - bottomMargin; 
+            bool isRightMost = tidx.global[1] >= computeDomain[1] - rightMargin; 
+            bool isBottomMost = tidx.global[0] >= computeDomain[0] - bottomMargin; 
 
             // Exit branching as quickly as possible as the majority of threads will not meet the boundary criteria
             if (isRightMost | isBottomMost)
             {
+                int idx0, idx1;
                 if (isRightMost)
                 {
-                    const int idx0 = tidx.global[0]; 
-                    const int idx1 = tidx.global[1] + rightMargin;
+                    idx0 = tidx.global[0]; 
+                    idx1 = tidx.global[1] + rightMargin;
                     outDataView(idx1, idx0) = inDataView(idx0, idx1);    
                 }
-
                 if (isBottomMost)
                 {
-                    const int idx1 = tidx.global[1];
-                    const int idx0 = tidx.global[0] + bottomMargin;
+                    idx1 = tidx.global[1];
+                    idx0 = tidx.global[0] + bottomMargin;
                     outDataView(idx1, idx0) = inDataView(idx0, idx1);
                 }
-
                 if (isRightMost && isBottomMost) 
                 { 
-                    const int idx0 = tidx.global[0] + bottomMargin;
-                    const int idx1 = tidx.global[1] + rightMargin;
+                    idx0 = tidx.global[0] + bottomMargin;
+                    idx1 = tidx.global[1] + rightMargin;
                     outDataView(idx1, idx0) = inDataView(idx0, idx1);
                 }
             }
