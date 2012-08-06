06d74857e87630a62a0273cf97c1050d1f1dd58d
AdeMiller_cp <SND\AdeMiller_cp>
2012-08-05 18:40:15 -0700

    FINISHED CODE SYNC FOR CH. 4.

--- Samples/Chapter4/main.cpp
+++ Samples/Chapter4/main.cpp
@@ -25,6 +25,75 @@
 
 using namespace concurrency;
 
+//--------------------------------------------------------------------------------------
+//  GPU simple matrix multiply
+//--------------------------------------------------------------------------------------
+
+void MatrixMultiply(std::vector<float>& vC, 
+         const std::vector<float>& vA, 
+         const std::vector<float>& vB, int M, int N, int W)
+{
+    array_view<const float,2> a(M, W, vA);
+    array_view<const float,2> b(W, N, vB);
+    array_view<float,2> c(M, N, vC);
+    c.discard_data(); 
+    parallel_for_each(c.extent, [=](index<2> idx) restrict(amp) 
+    {
+        int row = idx[0]; 
+        int col = idx[1];
+        float sum = 0.0f;
+        for(int i = 0; i < W; i++)
+            sum += a(row, i) * b(i, col);
+        c[idx] = sum;
+    });
+    c.synchronize();
+}
+
+//--------------------------------------------------------------------------------------
+//  GPU tiled matrix multiply
+//--------------------------------------------------------------------------------------
+
+static const int TileSize = 16;
+
+void MatrixMultiplyTiled(std::vector<float>& vC, 
+         const std::vector<float>& vA, 
+         const std::vector<float>& vB, 
+         int M, int N, int W)
+{
+    array_view<const float,2> a(M, W, vA);
+    array_view<const float,2> b(W, N, vB);
+    array_view<float,2> c(M, N, vC);
+    c.discard_data();
+
+    parallel_for_each(c.extent.tile< TileSize, TileSize >(),
+        [=] (tiled_index< TileSize, TileSize> tidx) restrict(amp) 
+    {
+        int row = tidx.local[0]; 
+        int col = tidx.local[1];
+        float sum = 0.0f;
+        for (int i = 0; i < W; i += TileSize) 
+        {
+            tile_static float sA[TileSize][TileSize];
+            tile_static float sB[TileSize][TileSize];
+            sA[row][col] = a(tidx.global[0], col + i);
+            sB[row][col] = b(row + i, tidx.global[1]);
+
+            tidx.barrier.wait();
+
+            for (int k = 0; k < TileSize; k++)
+                sum += sA[row][k] * sB[k][col];
+
+            tidx.barrier.wait();
+        }
+        c[tidx.global] = sum;
+    });
+    c.synchronize();
+}
+
+//--------------------------------------------------------------------------------------
+//  GPU simple matrix multiply functor
+//--------------------------------------------------------------------------------------
+
 class Multiply
 {
 private:
@@ -53,7 +122,7 @@ public:
 int main()
 {
     const int M = 64;
-    const int N = 64;
+    const int N = 64 * 64;
     const int W = 64;
 
     accelerator defaultDevice(accelerator::default_accelerator);
@@ -89,70 +158,29 @@ int main()
             for(int col = 0; col < N; ++col)
             {
                 float result = 0.0f;
-
                 for(int i = 0; i < W; ++i)
                 {
                     int idxA = row * W + i;
                     int idxB = i * N + col;
-
                     result += vA[idxA] * vB[idxB];
                 }
-
                 vRef[row * N + col] = result;
             }
         }
     });
 
-    std::wcout << "CPU exec time: " << elapsedTime << " (ms)" << std::endl << std::endl;
+    std::wcout << "CPU exec time: " << elapsedTime << " (ms)" << std::endl;
 
     //--------------------------------------------------------------------------------------
-    //  GPU non tiled matrix multiply with functors
-    //--------------------------------------------------------------------------------------
-
-    elapsedTime = TimeFunc([&]()
-    {
-        extent<2> eA(M, W), eB(W, N), eC(M, N);
-        array_view<float, 2> mA(eA, vA); 
-        array_view<float, 2> mB(eB, vB); 
-        array_view<float, 2> mC(eC, vC);
-
-        mC.discard_data();
-        parallel_for_each(defaultView, extent<2>(eC), Multiply(mA, mB, mC, W));
-
-        mC.synchronize();
-    });
-
-    //--------------------------------------------------------------------------------------
-    //  GPU non tiled matrix multiply
+    //  GPU simple matrix multiply
     //--------------------------------------------------------------------------------------
 
     elapsedTime = TimeFunc([&]()
     {
-        extent<2> eA(M, W), eB(W, N), eC(M, N);
-        array_view<float, 2> mA(eA, vA); 
-        array_view<float, 2> mB(eB, vB); 
-        array_view<float, 2> mC(eC, vC);
-
-        mC.discard_data();
-        parallel_for_each(defaultView, extent<2>(eC), [=](index<2> idx) restrict(amp)
-        {
-            float result = 0.0f;
-
-            for(int i = 0; i < mA.extent[1]; ++i)
-            {
-                index<2> idxA(idx[0], i);
-                index<2> idxB(i, idx[1]);
-
-                result += mA[idxA] * mB[idxB];
-            }
-
-            mC[idx] = result;
-        });
-
-        mC.synchronize();
+        MatrixMultiply(vC, vA, vB, M, N, W);
     });
 
-    std::wcout << "GPU exec time (non tiled) including copy-in/out: " << elapsedTime << " (ms)" << std::endl << std::endl;
+    std::wcout << std::endl << "GPU exec time (non tiled) including copy-in/out: " << elapsedTime << " (ms)" << std::endl;
 
     // Compare GPU non tiled and CPU resulTileSize
 
@@ -162,49 +190,53 @@ int main()
         size_t i = std::distance(vC.cbegin(), firstMismatch.first);
         std::wcout << "vC[" << i << "] = " << *firstMismatch.first << ", vRef[" << i << "] = " << *firstMismatch.second << std::endl;
     }
-    std::wcout << " non tiled " << ((firstMismatch.first == vC.end()) ? "PASSED" : "FAILED") << std::endl << std::endl;
+    std::wcout << " non tiled " << ((firstMismatch.first == vC.end()) ? "PASSED" : "FAILED") << std::endl;
 
     //--------------------------------------------------------------------------------------
     //  GPU tiled matrix multiply
     //--------------------------------------------------------------------------------------
 
-    static const int TileSize = 16;
-
     static_assert((M % TileSize == 0) && (W % TileSize == 0) && (N % TileSize == 0), "Matrix dimensions must be a multiple of tile size.");
 
     elapsedTime = TimeFunc([&]()
     {
-        array_view<const float,2> a(M, W, vA);
-        array_view<const float,2> b(W, N, vB);
-        array_view<float,2> c(M, N, vC);
-
-        c.discard_data();
-        parallel_for_each(c.extent.tile<TileSize, TileSize>(),
-            [=] (tiled_index<TileSize, TileSize> tidx) restrict(amp) 
-        {
-            int row = tidx.local[0]; 
-            int col = tidx.local[1];
-            float sum = 0.0f;
-            for (int i = 0; i < W; i += TileSize) 
-            {
-                tile_static float sA[TileSize][TileSize], sB[TileSize][TileSize];
-                sA[row][col] = a(tidx.global[0], col + i);
-                sB[row][col] = b(row + i, tidx.global[1]);
-                tidx.barrier.wait();
-                for (int k = 0; k < TileSize; k++)
-                    sum += sA[row][k] * sB[k][col];
-                tidx.barrier.wait();
-            }
-            c[tidx.global] = sum;
-        });
-
-        c.synchronize();
+        MatrixMultiplyTiled(vC, vA, vB, M, N, W);
     });
 
-    std::wcout << "GPU exec time (tiled - tile size is " << TileSize << ") including copy-in/out: " <<
-        elapsedTime << " (ms)" << std::endl << std::endl;
+    std::wcout << std::endl << "GPU exec time (tiled - tile size is " << TileSize << ") including copy-in/out: " <<
+        elapsedTime << " (ms)" << std::endl;
 
     // Compare tiled GPU and CPU results
+
+    firstMismatch = std::mismatch(vC.cbegin(), vC.cend(), vRef.cbegin(), [](float c, float r) { return (fabs(c - r) < 0.01); });
+    if (firstMismatch.first != vC.end())
+    {
+        size_t i = std::distance(vC.cbegin(), firstMismatch.first);
+        std::wcout << "vC[" << i << "] = " << *firstMismatch.first << ", vRef[" << i << "] = " << *firstMismatch.second << std::endl;
+    }
+    std::wcout << " tiled " << ((firstMismatch.first == vC.end()) ? "PASSED" : "FAILED") << std::endl;
+
+    //--------------------------------------------------------------------------------------
+    //  GPU simple matrix multiply with functors
+    //--------------------------------------------------------------------------------------
+
+    elapsedTime = TimeFunc([&]()
+    {
+        extent<2> eA(M, W), eB(W, N), eC(M, N);
+        array_view<float, 2> mA(eA, vA); 
+        array_view<float, 2> mB(eB, vB); 
+        array_view<float, 2> mC(eC, vC);
+        mC.discard_data();
+
+        parallel_for_each(defaultView, extent<2>(eC), Multiply(mA, mB, mC, W));
+
+        mC.synchronize();
+    });
+
+    std::wcout << std::endl << "GPU functor exec time including copy-in/out: " <<
+        elapsedTime << " (ms)" << std::endl;
+
+    // Compare simple GPU functor and CPU results
 
     firstMismatch = std::mismatch(vC.cbegin(), vC.cend(), vRef.cbegin(), [](float c, float r) { return (fabs(c - r) < 0.01); });
     if (firstMismatch.first != vC.end())
