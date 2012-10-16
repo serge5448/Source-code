29191aaf679a94f01601c22ee6eb63eb77f7229f
AdeMiller_cp <SND\AdeMiller_cp>
2012-10-16 13:56:45 -0700

    Added String Reversal with C++ AMP to Extras.

---
+++ Extras/ReverseString/ReverseStr.h
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#pragma once
+
+namespace Examples
+{
+    void ReverseStr(char* const pStr);
+    void ReverseStrAmp(char* const pStr);
+}
---
+++ Extras/ReverseString/ReverseStrTests.cpp
@@ -0,0 +1,134 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "ReverseStr.h"
+
+using namespace concurrency;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Examples;
+
+namespace ReverseStringTests
+{		
+    std::wstring Msg(std::string& expected, std::string& actual)
+    {
+        std::wostringstream msg;
+        msg << "'" << std::wstring(expected.cbegin(), expected.cend()) <<  "' != '" << std::wstring(actual.cbegin(), actual.cend()) << "'" << std::endl;
+        return msg.str();
+    }
+
+    TEST_CLASS(ReverseStrTests)
+    {
+    public:
+        TEST_METHOD(ReverseStrTests_SimpleString)
+        {
+            std::string input("abc");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrTests_SingleChar)
+        {
+            std::string input("a");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrTests_OddNumberOfChars)
+        {
+            std::string input("abc");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrTests_EmptyString)
+        {
+            std::string input("");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+    };
+
+    TEST_CLASS(ReverseStrAmpTests)
+    {
+    public:
+        TEST_CLASS_INITIALIZE(ReverseStrAmpTests_InitializeAmp) 
+        {
+            // Initialize the C++ AMP runtime. This ensures that the first test run doesn't seem really slow.
+            array<int> data(100);
+            parallel_for_each(extent<1>(1), [&data](index<1> idx) restrict(amp) { data[idx] = 0; });
+            accelerator(accelerator::default_accelerator).default_view.wait();
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_SimpleString)
+        {
+            std::string input("abcdabcd");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStrAmp(const_cast<char*>(input.c_str()));
+            
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_SimpleUnbalancedString)
+        {
+            std::string input("abcdabcdab");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStrAmp(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_SingleChar)
+        {
+            std::string input("a");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_OddNumberOfCharsSubBlockSize)
+        {
+            std::string input("abc");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_OddNumberOfChars)
+        {
+            std::string input("abcdefghijklmnop");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+
+        TEST_METHOD(ReverseStrAmpTests_EmptyString)
+        {
+            std::string input("");
+            std::string expected(input.rbegin(), input.rend());
+
+            ReverseStr(const_cast<char*>(input.c_str()));
+
+            Assert::AreEqual(0, expected.compare(input), Msg(expected, input).c_str());
+        }
+    };
+}
